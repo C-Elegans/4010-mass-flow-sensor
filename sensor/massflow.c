e59a856eea2458d9e8994da0ebdf84ed3a0fe0fa
Michael Nolan <mtnolan2640@gmail.com>
2020-04-05 13:27:57 -0400

    Add tokelvin function

--- sensor/massflow.c
+++ sensor/massflow.c
@@ -11,14 +11,15 @@ This function calculates the Heat Capacities of Air (Cp).
 Made a statistical plot, based on the heat capacities or air table at
 https://www.ohio.edu/mechanical/thermo/property_tables/air/air_Cp_Cv.html.
 Found the best fitted cubic function according to it's data.
-Temperature needs to be in Kelvin.
+Temperature needs to be in Celcius
 */
 
 float get_heat_capacity(float temp)
 {
-    float cp = ((-1.29142e-10)*powf(temp, 3.0)) +
-	       ((3.2309586e-7)*powf(temp, 2.0)) +
-	       (-4.703434e-5*temp) +
+    float tempk = tokelvin(temp);
+    float cp = ((-1.29142e-10)*powf(tempk, 3.0)) +
+	       ((3.2309586e-7)*powf(tempk, 2.0)) +
+	       (-4.703434e-5*tempk) +
 	        0.9917484975;
     return cp;
 }
@@ -36,6 +37,10 @@ float get_mass_flow(float temp1, float temp2)
     float M=(K*HEATER_POWER)/(cp*dtemp);
 
     return M;
+}
+
+float tokelvin(float tempc){
+    return tempc + 273.15;
 }
 
 /* Spencer, you want to do these? you should be able to adapt the code you have from your 4020 lab for these. IDK how you connected yours, but the ones on our tube are connected like so:
--- sensor/massflow.h
+++ sensor/massflow.h
@@ -1,3 +1,5 @@
+float tokelvin(float);
+
 float get_mass_flow(float temp1, float temp2);
 
 float get_temperature(int thermistor_number);
