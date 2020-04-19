334d86f0a0bf137fd982c61c65b36abd51d8d040
Michael Nolan <mtnolan2640@gmail.com>
2020-04-19 11:24:49 -0400

    Add PID controller

--- sensor/pwmthread.c
+++ sensor/pwmthread.c
@@ -15,9 +15,31 @@
 
 float mass_flow_rate = 0;
 
-
 float ambient_temp;
 int pwm_duty_cycle = 0;
+
+
+float kp = 500.0;
+float ki = 0.0;
+float kd = 0.0;
+
+static float integral_error = 0;
+static float past_error = 0;
+void get_pwm_duty_cycle(float absdiff){
+
+    float error = DT_TARGET - absdiff;
+
+    pwm_duty_cycle = kp * error + ki * integral_error
+	- kd * past_error; 
+
+    if(pwm_duty_cycle < 0) pwm_duty_cycle = 0;
+    if(pwm_duty_cycle > PWM_PERIOD) pwm_duty_cycle = PWM_PERIOD;
+    
+    integral_error += absdiff;
+    past_error = absdiff;
+}
+
+
 void pwm_thread(void* data){
     struct pwm_context *ctx =  create_pwm_context();
     set_period(ctx, PWM_PERIOD);
@@ -30,23 +52,11 @@ void pwm_thread(void* data){
 	float t1 = get_temperature(temp1);
 	float t2 = get_temperature(temp2);
 	float absdiff = fabsf(t1-t2);
-	float mintempdiff = fabsf(fminf(t1, t2) - ambient_temp);
+	//float mintempdiff = fabsf(fminf(t1, t2) - ambient_temp);
 	bool no_flow = false;
 
+	get_pwm_duty_cycle(absdiff);
 
-	if(mintempdiff >2 && absdiff < DT_TARGET){
-	    pwm_duty_cycle -= PWM_STEP;
-	    no_flow = true;
-	}
-	else {
-	    pwm_duty_cycle += (DT_TARGET - absdiff) * PWM_STEP;
-	}
-	if(pwm_duty_cycle < 0)
-	    pwm_duty_cycle = 0;
-	if(pwm_duty_cycle > PWM_PERIOD)
-	    pwm_duty_cycle = PWM_PERIOD;
-
-				
 	if(no_flow){
 	    mass_flow_rate = 0;
 	}
